b8386bd51bf4b2fa4d141933c747adce8de79458
Danny Schreiber <danny@ravenartmedia.com>
2015-02-23 13:51:01 -0800

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -8,14 +8,10 @@ var rootPath = path.normalize(__dirname  + '../../../');
 module.exports = {
     development: {
         rootPath: rootPath,
-        baseUrl: 'http://localhost:3002',
-        db: 'mongodb://dannyschreiber.net/chowlio',
         port: process.env.PORT || 3002
     },
     production: {
         rootPath: rootPath,
-        baseUrl: 'http://admin.abracadaniel.net',
-        db: 'mongodb://heroku_app33803161:hf0ai7fhuiroib66egrflg7h28@ds041851.mongolab.com:41851/heroku_app33803161',
         port: process.env.PORT || 80
     }
 };
